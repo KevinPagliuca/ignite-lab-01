ce55e514c72e5f58c9805b6bd225b83bcaaaab05
Kevin Pagliuca <kevin.pagliuca@outlook.com>
2022-04-12 13:36:29 -0300

    Aula 04 - API Integrate with SSR

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React from "react";
 import {
   Claims,
   getSession,
   useUser,
   withPageAuthRequired,
-} from '@auth0/nextjs-auth0';
-import Link from 'next/link';
+} from "@auth0/nextjs-auth0";
+import Link from "next/link";
+import { withApollo } from "../../lib/withApollo";
+import {
+  getServerPageGetProducts,
+  ssrGetProducts,
+} from "../../graphql/generated/page";
+import { useMeQuery } from "../../graphql/generated/graphql";
 
-interface HomeProps {
-  user: Claims;
-  accessToken: string;
-}
+function Home({ data }) {
+  const { user } = useUser();
+  const { data: me } = useMeQuery();
 
-export default function Home({ user, accessToken }: HomeProps) {
-  console.log(accessToken);
   return (
-    <div>
+    <div className="text-violet-500">
       <h1>Home</h1>
+      <pre>{JSON.stringify(me, null, 2)}</pre>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
       <pre>{JSON.stringify(user, null, 2)}</pre>
       <Link href="/api/auth/logout">Logout</Link>
     </div>
@@ -24,8 +29,12 @@ export default function Home({ user, accessToken }: HomeProps) {
 }
 
 export const getServerSideProps = withPageAuthRequired({
-  getServerSideProps: async ctx => {
-    const { accessToken, user } = getSession(ctx.req, ctx.res);
-    return { props: { accessToken, user } };
+  getServerSideProps: async (ctx) => {
+    // return getServerPageGetProducts({}, ctx);
+    return {
+      props: {},
+    };
   },
 });
+
+export default withApollo(ssrGetProducts.withPage()(Home));
